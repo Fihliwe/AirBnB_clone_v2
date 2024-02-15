45a6012c43c3afa05a3dc75b239de02019e2d6b5
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-02-15 13:00:08 +0000

    modified file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Pyhton script that starts a flask web application
+Python script that starts a Flask web application
 """
 from flask import Flask
-
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """
-    return hello HBNB!
+    Return 'Hello HBNB!'
     """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', post=5000)
+    print("* Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)")
+    app.run(host='0.0.0.0', port=5000)
