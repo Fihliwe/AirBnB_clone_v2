fc10f0fa29801bf247a10d9be19f96f7f5e1046c
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-02-17 16:17:04 +0000

    added python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,16 +22,31 @@ def display_hbnb():
     """
     return "HBNB"
 
-@app.route("/c/<text>", strict_slashes=False)
-def display_c(text):
+@app.route("/c/is_fun", strict_slashes=False)
+def display_c():
     """
     return “C ” followed by the value of the text variable
     """
+    return "C is fun"
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c_is_cool(text):
+    """
+    return c cool
+    """
     return 'C ' + text.replace('_', ' ')
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def display_is_magic(text):
+    """
+    return "is magic"
+    """
+    return 'Python ' + text.replace('_', ' ')
 
 if __name__ == "__main__":
     """
-    run above code
+    main console
     """
     
     print("* Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)")
